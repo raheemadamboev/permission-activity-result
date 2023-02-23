4756f182146beb3fec925d380feee0c3e7ee8f4b
raheemadamboev <vividricemaryandmax@gmail.com>
2023-02-23 22:29:56 +0500

    Added permissions;

---
+++ app/src/main/java/xyz/teamgravity/permissionactivityresult/Permissions.kt
@@ -0,0 +1,10 @@
+package xyz.teamgravity.permissionactivityresult
+
+import android.Manifest
+
+object Permissions {
+    val value = arrayOf(
+        Manifest.permission.RECORD_AUDIO,
+        Manifest.permission.CALL_PHONE,
+    )
+}
